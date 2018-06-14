4a5e06e242fcf2307ba0803c81d89dbcafc6002c
bilojan91@gmial.com <bilojan91@gmial.com>
2018-06-14 10:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // write your code below!
-Function happyHolidays(){
+Function happyHolidayTo(){
   return "happyHolidays";
+}
+function happyHolidayTo(name){
+  return`happyHolidays, ${name}!`;
+}
+function happyHolidayTo(hoilday,name){
+  return `happy${hoilday},${name};
+}
+
+function holidayCountdown(holiday,name){
+  return `it is ${name}days until ${holiday}!`;
 }
